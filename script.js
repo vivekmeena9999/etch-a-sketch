e21153119332fe639565e7f3d752636f77ab9626
vivekmeena9999 <vivekkumarmeena999@gmail.com>
2026-08-10 09:23:07 +0530

    ADD THE BUTTON GRID MAKE FUNCTIONALITY
    Added the serGrid() and cleaner() function to implement
    the functionality of making custom gird by entering a
    value in the prompt.

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 let container = document.querySelector(".container")
-for (let i=1; i<=256;i++){
-    let div= document.createElement("div")
-    div.style.width="31.25px"
-    div.style.height="31.25px"
-    div.style.border="1px solid"
-    div.style.borderColor="black"
-    div.style.boxSizing="border-box"
-    
-// For next vivek:- brother i am not able to understand the formula for a perfect fit, like the relation between the no. of grids that a user wants and the percentage to set fot it like for 16x16 the percentage is 5.5% like why and how?
-// solved the box sizing property's value was set to borderBox instead of border-box
-    
-    div.classList.add(`_${i}`)
-    container.appendChild(div)
+let btn = document.getElementById("grid-set")
+btn.addEventListener("click", ()=>{setGrid(true)})
+
+function gridMaker(num) {
+    for (let i = 1; i <= (num * num); i++) {
+        let div = document.createElement("div")
+        div.style.width = `${650 / num}px`
+        div.style.height = `${650 / num}px`
+        //div.style.border = "0.5px solid"
+        //div.style.borderColor = "white"
+        div.style.boxSizing = "border-box"
+
+
+
+        div.classList.add(`_${i}`)
+        container.appendChild(div)
+    }
 }
-function colorChange(event){
-    console.log(event.target.classList)
-elemClass= event.target.classList
-if (elemClass!= "container"){
-let elem = document.querySelector(`.${elemClass}`)
-elem.style.backgroundColor="red";}
+function cleaner(){
+    
+    container.replaceChildren()
+
+
 }
-container.addEventListener("mouseover",colorChange )
+function colorChange(event) {
+   
+    elemClass = event.target.classList
+    if (elemClass != "container") {
+        let elem = document.querySelector(`.${elemClass}`)
+        elem.style.backgroundColor = "red";
+    }
+}
+function setGrid(bool) {
+    if (bool) {
+        cleaner()
+        let num = prompt("Provide the desired Square per Grid")
+        if (num > 100) {
+            let num = prompt("Value must be smaller than 100, enter again")
+
+        }
+        gridMaker(Number(num));
+    }
+    else{
+        gridMaker(16);
+    }
+}
+setGrid(false)
+container.addEventListener("mouseover", colorChange)
