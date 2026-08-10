5a2aeb96119ad2330f5f0d3f3df8ade26a2d4842
vivekmeena9999 <vivekkumarmeena999@gmail.com>
2026-08-10 17:58:11 +0530

    ADD THE DARKENING FUNCTIONALITY OVER EACH CLICK
    Added the darkening ability, had to use some ai to find out the
    mathematical way to do this.
    
    How I did this:- Made a passesArr Array which keeps track of
    each time the div is hovered, and also updates it's value by 1 each time.
    
    Then calculated another variable name factor, which is (10 - passesArr[element_index]) / 10
    Now each time the div is hovered the factor gets reduced by 0.1.
    Now plugged this factor to multipy it with each random number generator, to
    sort of crank the value down by that times.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let container = document.querySelector(".container")
 let btn = document.getElementById("grid-set")
 btn.addEventListener("click", ()=>{setGrid(true)})
-
+let passesArr= []
 function gridMaker(num) {
+    passesArr= Array((num*num)).fill(null)
     for (let i = 1; i <= (num * num); i++) {
         let div = document.createElement("div")
         div.style.width = `${650 / num}px`
@@ -27,8 +28,12 @@ function colorChange(event) {
    
     elemClass = event.target.classList
     if (elemClass != "container") {
+        let elemID= elemClass[0].slice(1);
+        if(passesArr[elemID] <10 ){
+        passesArr[elemID] += 1
+        }
         let elem = document.querySelector(`.${elemClass}`)
-        elem.style.backgroundColor = randomColorGenerator();
+        elem.style.backgroundColor = randomColorGenerator(elemID);
     }
 }
 function setGrid(bool) {
@@ -48,8 +53,14 @@ function setGrid(bool) {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function randomColorGenerator(){
-    return `rgb( ${getRandomInt(0,255)} , ${getRandomInt(0,255)},${getRandomInt(0,255)} )`
+function randomColorGenerator(index){
+    let factor = (10 - passesArr[index])/10
+    if(factor >= 0){
+    return `rgb( ${getRandomInt(0,255) *factor}
+     , ${getRandomInt(0,255)* factor}
+     ,${getRandomInt(0,255)* factor} )`
+    }
+    return 'rgb(0,0,0)'
 }
 setGrid(false)
 container.addEventListener("mouseover", colorChange)
